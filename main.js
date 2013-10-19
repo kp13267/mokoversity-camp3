fb1e4a217afb52e73e72dd013a5c906fc73d98de
kp13267 <kp13267@yahoo.com.tw>
2013-10-19 14:45:23 +0800

    fix #6

---
+++ main.js
@@ -0,0 +1,15 @@
+(function(){
+var canvas = document.getElementById('game');
+var ctx = canvas.getContext('2d');
+
+	var ballx = Math.floor(Math.random() * 300);// 0 - 300
+	var bally = Math.floor(Math.random() * 500);// 0 - 500
+	var ballz = Math.floor(Math.random() * 100);// 0 - 100
+canvas.width = 480;
+canvas.height = 320;
+
+ctx.fillstyle = 'black';
+ctx.beginPath();
+ctx.arc(ballx, bally, ballz, 0, Math.PI * 2 , true);
+ctx.fill();
+}) ();
