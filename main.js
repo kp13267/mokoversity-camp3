70fd3b833a7c601c9f22caf23025339897a1f723
kp13267 <kp13267@yahoo.com.tw>
2013-10-20 12:02:04 +0800

    fix #11

--- main.js
+++ main.js
@@ -1,61 +1,65 @@
 var gameModule = (function(){
 
 	var timeoutVar,
+		
 		counter = 0,
 		ballX,
 		ballY,
 		ballR,
+		scores,
 		colors = ['#ff0000','#000ff','yellow'],
 		length = colors.length;
 
 
-function touchEvent(evt){
-	var x= evt.clientX,
-	    y= evt.clientY;
-	    tmp = (ballX - x) * (ballX - x)+(ballY - y) * (ballY- y);
+	function touchEvent(evt){
+		var x= evt.clientX,
+		    y= evt.clientY;
+		    tmp = (ballX - x) * (ballX - x)+(ballY - y) * (ballY- y);
 
-	console.log("Clicked: " + x + " , " + y);
-	
-	if(tmp < ballR * ballR){
-		console.log("Hit ! Your scores: "+ scores);
-	}
+		console.log("Clicked: " + x + " , " + y);
+
+		if(tmp < ballR * ballR) {
+			scores = scores + (100 - ballR);
+			console.log("Hit ! Your scores: " + scores);
+		  }
 	}
 
 	function start(){
+	    scores = 0;
 
-	document.getElementById("main").addEventListener("click",touchEvent,false);
-	startGame();
+		document.getElementById("main").addEventListener("click", touchEvent, false);
+		startGame();
 	}
 
 	function startGame(){
-var canvas = document.getElementById('game');
-var ctx = canvas.getContext('2d');
-	var ballX = Math.floor(Math.random() * 300);// 0 - 300
-	var ballY = Math.floor(Math.random() * 500);// 0 - 500
-	var ballR = Math.floor(Math.random() * 100);// 0 - 100
+		var canvas = document.getElementById('game');
+		var ctx = canvas.getContext('2d');
+			ballX = Math.floor(Math.random() * 300);// 0 - 300
+			ballY = Math.floor(Math.random() * 500);// 0 - 500
+			ballR = Math.floor(Math.random() * 100);// 0 - 100
 
-canvas.width = 480;
-canvas.height = 320;
+		canvas.width = 480;
+		canvas.height = 320;
 
-ctx.fillstyle = 'black';
-ctx.beginPath();
-ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
-ctx.fill();
+		ctx.fillstyle = 'black';
+		ctx.beginPath();
+		ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
+		ctx.fill();
 
-if (counter >= 10){
+		if (counter >= 10){
 
-} else {
+		} else {
 
-	timeoutVar = setTimeout(startGame, 1000);
-	counter = counter + 1;
+			timeoutVar = setTimeout(startGame, 1000);
+			counter = counter + 1;
 
-	console.log("Counter: " + counter);
-}
+			console.log("Counter: " + counter);
+		}
 
-}
+	}
 
 function gameOver(){
-	//console.log("Coucter: " + counter);
+	console.log("Final: " + scores);
 }
 
 return{
